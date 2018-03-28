c6412897d2f91405988aebc9c7e559ceaa776e97
= <grench=>
2018-03-29 01:35:53 +0900

    make AbstractFactort(facotryPackage) 1/2

---
+++ GofDesignPattern/src/ch08_abstractFactory/factory/Item.java
@@ -0,0 +1,10 @@
+package ch08_abstractFactory.factory;
+
+public abstract class Item {
+	protected String caption;
+
+	public Item(String caption) {
+		this.caption =caption;
+	}
+	public abstract String makeHTML();
+}
---
+++ GofDesignPattern/src/ch08_abstractFactory/factory/Link.java
@@ -0,0 +1,10 @@
+package ch08_abstractFactory.factory;
+
+public abstract class Link extends Item{
+	protected String url;
+	
+	public Link(String caption,String url) {
+		super(caption);
+		this.url = url;
+	}
+}
