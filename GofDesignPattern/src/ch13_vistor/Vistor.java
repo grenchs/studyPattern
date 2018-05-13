46d25b8a6a4627c42aa98ce6e72f5bc43955e263
grench <grench1397@gmail.com>
2018-05-13 14:23:45 +0900

    test Branch to master

--- GofDesignPattern/src/ch13_vistor/Vistor.java
+++ GofDesignPattern/src/ch13_vistor/Vistor.java
@@ -2,5 +2,5 @@ package ch13_vistor;
 
 public abstract class Vistor {
     public abstract void visit(File file);
-    public abstract void visit(Directory);
+    public abstract void visit(Directory directory);
 }
