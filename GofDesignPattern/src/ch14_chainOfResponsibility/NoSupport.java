b736e6fe14667c745c025ba758ed9b44f2316e3e
grench <grench1397@gmail.com>
2018-05-26 23:35:51 +0900

    complete ch14

---
+++ GofDesignPattern/src/ch14_chainOfResponsibility/NoSupport.java
@@ -0,0 +1,12 @@
+package ch14_chainOfResponsibility;
+
+public class NoSupport extends Support {
+
+    public NoSupport(String name) {
+        super(name);
+    }
+    
+    protected boolean resolve(Trouble trouble) {
+        return false;
+    }
+}
---
+++ GofDesignPattern/src/ch14_chainOfResponsibility/Trouble.java
@@ -0,0 +1,19 @@
+package ch14_chainOfResponsibility;
+
+public class Trouble {
+    
+    private int number;
+    
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+   
+    @Override
+    public String toString() {
+        return "[Trouble " + number + "]";
+    }
+}
